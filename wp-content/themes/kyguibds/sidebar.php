42126fec9fb65f6cb80a951a385bd38bc3a0a9f1
unknown <duynn250390@gmail.com>
2019-09-27 09:01:42 +0700

    ekululi

--- wp-content/themes/kyguibds/sidebar.php
+++
@@ -1,7 +0,0 @@
-<?php
-        if ( is_active_sidebar('sidebar-1') ) {
-                dynamic_sidebar( 'sidebar-1' );
-        } else {
-                _e('This is widget area. Go to Appearance -> Widgets to add some widgets.', 'thachpham');
-        }
-?>
